80fb6cfcf8f030aa086210342e67976070c1b8d8
Throuz <zed2786328@gmail.com>
2025-04-16 09:56:21 +0800

    fix hydration

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,7 @@
 
 import React from "react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import {
-  WalletModalProvider,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import {
   ConnectionProvider,
   WalletProvider,
@@ -15,6 +12,15 @@ import { DashboardLayout } from "./components/DashboardLayout";
 import { clusterApiUrl } from "@solana/web3.js";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import { dummyUser, dummyLeaderboard } from "./data/dummyData";
+import dynamic from "next/dynamic";
+
+const WalletMultiButton = dynamic(
+  () =>
+    import("@solana/wallet-adapter-react-ui").then(
+      (mod) => mod.WalletMultiButton
+    ),
+  { ssr: false }
+);
 
 export default function Home() {
   const network = WalletAdapterNetwork.Devnet;
